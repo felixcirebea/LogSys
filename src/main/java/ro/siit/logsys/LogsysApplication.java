d0fc5d438431b373704a6620be236eb0303c7052
felixcirebea <felix.cirebea@gmail.com>
2023-03-08 16:33:55 +0200

    Initial commit

---
+++ src/main/java/ro/siit/logsys/LogsysApplication.java
@@ -0,0 +1,13 @@
+package ro.siit.logsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogsysApplication.class, args);
+	}
+
+}
