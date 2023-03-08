656499ecd83a5199f31b8ad39c83be191702b142
Ene_A <soniarosianu@yahoo.com>
2023-03-08 17:47:20 +0200

    Implement LS08

---
+++ src/main/java/ro/siit/logsys/helper/CompanyInfoContributor.java
@@ -0,0 +1,34 @@
+package ro.siit.logsys.helper;
+
+import lombok.Data;
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@Data
+public class CompanyInfoContributor implements InfoContributor {
+    private Integer totalProfit = 0;
+    private LocalDate currentDate = LocalDate.of(2021, 12, 14);
+
+    @Override
+    public void contribute(Info.Builder builder) {
+        Map<String, String> companyStats = new HashMap<>();
+        companyStats.put("company-profit", String.valueOf(totalProfit));
+        companyStats.put("current-date", String.valueOf(currentDate));
+
+        builder.withDetail("company", companyStats);
+    }
+
+    public synchronized void addProfit(Integer value) {
+        this.totalProfit += value;
+    }
+
+    public synchronized void incrementDate() {
+        this.currentDate = currentDate.plusDays(1);
+    }
+}
